6cefc4f5317a8cfcfde7f2a5afb4ddfbcabae157
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-12-25 22:26:15 -0800

    Remove achievement table

--- LodestoneDataExporter/Program.cs
+++ LodestoneDataExporter/Program.cs
@@ -19,53 +19,11 @@ namespace LodestoneDataExporter
             var cyalume = new Cyalume(dataPath);
 
             await Task.WhenAll(
-                Task.Run(() => ExportAchievementTable(cyalume)),
                 Task.Run(() => ExportItemTable(cyalume)),
                 Task.Run(() => ExportMinionTable(cyalume)),
                 Task.Run(() => ExportMountTable(cyalume)),
                 Task.Run(() => ExportTitleTable(cyalume))
             );
-        }
-
-        private static void ExportAchievementTable(Cyalume cyalume)
-        {
-            var achievementTable = new AchievementTable { Achievements = new List<Achievement>() };
-            var languages = new[] { Language.English, Language.Japanese, Language.German, Language.French };
-            foreach (var lang in languages)
-            {
-                var achievementSheet = cyalume.GetExcelSheet<Lumina.Excel.GeneratedSheets.Achievement>(lang);
-                Parallel.ForEach(achievementSheet, new ParallelOptions { MaxDegreeOfParallelism = 4 }, achievement =>
-                {
-                    Achievement curAchievement;
-                    lock (achievementTable.Achievements)
-                    {
-                        curAchievement = achievementTable.Achievements.FirstOrDefault(a => a.Id == achievement.RowId);
-                        if (curAchievement == null)
-                        {
-                            curAchievement = new Achievement { Id = achievement.RowId };
-                            achievementTable.Achievements.Add(curAchievement);
-                        }
-                    }
-
-                    switch (lang)
-                    {
-                        case Language.English:
-                            curAchievement.NameEn = achievement.Name;
-                            break;
-                        case Language.Japanese:
-                            curAchievement.NameJa = achievement.Name;
-                            break;
-                        case Language.German:
-                            curAchievement.NameDe = achievement.Name;
-                            break;
-                        case Language.French:
-                            curAchievement.NameFr = achievement.Name;
-                            break;
-                    }
-                });
-            }
-
-            Serialize(Path.Join(OutputDir, "achievement_table.bin"), achievementTable);
         }
 
         private static void ExportItemTable(Cyalume cyalume)
